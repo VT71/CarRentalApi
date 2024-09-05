c42a32f35e034474ff3c39035138637e2f4704b6
VT71 <56217276+VT71@users.noreply.github.com>
2024-09-05 20:12:50 +0100

    Created first minimal version of Booking model.

---
+++ Models/Booking.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarRentalApi.Models;
+
+public class Booking
+{
+    public long Id { get; set; }
+    public DateTimeOffset PickUpDateTime { get; set; }
+    public DateTimeOffset DropOffDateTime { get; set; }
+    public long CarId { get; set; }
+    public required Car Car { get; set; }
+}
