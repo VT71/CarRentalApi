f0f006c720cec8a9bef195d2347305aa8258eb64
VT71 <56217276+VT71@users.noreply.github.com>
2024-09-11 14:56:01 +0100

    Added to relationship make - car restrict delete behavior.

--- Data/CarRentalContext.cs
+++ Data/CarRentalContext.cs
@@ -11,19 +11,21 @@ public class CarRentalContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-
+        modelBuilder.Entity<Car>().HasOne(c => c.Make).WithMany(m => m.Cars).HasForeignKey(c => c.MakeIf).OnDelete(DeleteBehavior.Restrict).IsRequired();
 
         modelBuilder.Entity<Booking>()
             .HasOne(b => b.PickUpLocation)
             .WithMany(l => l.PickUpBookings)
             .HasForeignKey(b => b.PickUpLocationId)
-            .IsRequired().OnDelete(DeleteBehavior.Restrict);
+            .OnDelete(DeleteBehavior.Restrict)
+            .IsRequired();
 
         modelBuilder.Entity<Booking>()
             .HasOne(b => b.DropOffLocation)
             .WithMany(l => l.DropOffBookings)
             .HasForeignKey(b => b.DropOffLocationId)
-            .IsRequired().OnDelete(DeleteBehavior.Restrict);
+            .OnDelete(DeleteBehavior.Restrict)
+            .IsRequired();
     }
 
     public DbSet<Car> Cars { get; set; }
